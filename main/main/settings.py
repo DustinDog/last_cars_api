dd3001217f0c0d2ab03b0f392587f81787ab500e
Vasyl Ihnatolia <vasyl.ihnat65@gmail.com>
2023-05-09 09:12:48 -0700

    add postgres support

--- main/main/settings.py
+++ main/main/settings.py
@@ -2,6 +2,10 @@ from datetime import timedelta
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,11 +121,13 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "mydatabase"),
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
