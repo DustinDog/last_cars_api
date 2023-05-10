3f1c7c9748be9560eabbe75dc46468a1454122a0
Yurii <yurii.yanochyk@optimile.eu>
2023-05-10 17:39:32 +0300

    IsCarOwner rewamp

--- main/cars/permissions.py
+++ main/cars/permissions.py
@@ -3,6 +3,8 @@ from cars.models import Car
 
 
 class IsCarOwner(BasePermission):
-    def has_permission(self, request, view):
-        car = Car.objects.get(pk=view.kwargs.get("pk"))
-        return car.user == request.user
+    def has_object_permission(self, request, view, obj):
+        return (
+            super().has_object_permission(request, view, obj)
+            and obj.user == request.user
+        )
