5a14964f6f3361904a4b430855ccf25229070846
Vasyl Ihnatolia <vasyl.ihnatolia@pettersonapps.com>
2023-05-17 09:44:40 +0000

    generate cars change

--- main/cars/filters.py
+++ main/cars/filters.py
@@ -4,16 +4,7 @@ from cars.models import Car, Model
 
 class ModelFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", help_text="the name of the model")
-    year_min = filters.NumberFilter(
-        field_name="year_of_issue",
-        lookup_expr="gte",
-        help_text="the year of issue of the model",
-    )
-    year_max = filters.NumberFilter(
-        field_name="year_of_issue",
-        lookup_expr="lte",
-        help_text="the year of issue of the model",
-    )
+
     body_style = filters.CharFilter(
         field_name="body_style",
         help_text="the body style of the model (hatcback, sedan, etc.)",
@@ -21,7 +12,7 @@ class ModelFilter(filters.FilterSet):
 
     class Meta:
         model = Model
-        fields = ["name", "year_min", "year_max", "body_style", "brand__name"]
+        fields = ["name", "body_style", "brand__name"]
 
 
 class CarFilter(filters.FilterSet):
@@ -35,17 +26,7 @@ class CarFilter(filters.FilterSet):
         lookup_expr="iexact",
         help_text="the name of the model",
     )
-    year_min = filters.NumberFilter(
-        field_name="model__year_of_issue",
-        lookup_expr="gte",
-        help_text="the year of issue of the model",
-    )
-    year_max = filters.NumberFilter(
-        field_name="model__year_of_issue",
-        lookup_expr="lte",
-        help_text="the year of issue of the model",
-    )
 
     class Meta:
         model = Car
-        fields = ["brand_name", "model_name", "year_min", "year_max"]
+        fields = ["brand_name", "model_name"]
