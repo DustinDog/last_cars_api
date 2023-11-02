835844936bd13fb6a9a56ce657d491970be3cbfc
vasyl <vasyl.ihnatolia@pettersonapps.com>
2023-11-02 17:02:42 +0200

    main branch on project

--- main/main/settings.py
+++ main/main/settings.py
@@ -147,13 +147,13 @@ CSRF_TRUSTED_ORIGINS = ["https://lobster-app-ku5vy.ondigitalocean.app"]
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "vasyl.ihnat65@gmail.com"
-EMAIL_HOST_PASSWORD = "luplprvtrxbtrqif"
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = "vasyl.ihnat65@gmail.com"
+EMAIL_HOST = os.environ.get("EMAIL_HOST"),
+EMAIL_PORT = os.environ.get("EMAIL_PORT"),
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER"),
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD"),
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS"),
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL"),
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL"),
 
 PASSWORD_RESET_TIMEOUT = 60 * 60
 
